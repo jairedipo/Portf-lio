0bcb3500bc791043283ba3a30a028662f49e8d99
Jair Edipo <edipo.jair@aluno.ufabc.edu.br>
2022-11-19 12:58:53 -0300

    Cadastro de pedido do form no banco de dados

--- Projetos/Cadastro de pedidos com Docker/app/cadastroPedidos.py
+++ Projetos/Cadastro de pedidos com Docker/app/cadastroPedidos.py
@@ -1,11 +1,27 @@
+import psycopg2
 from bottle import route, run, request
+
+DSN = 'dbname=jf_brinquedos user=postgres password=teste123 host=db'
+SQL = 'INSERT INTO pedido (data_compra, id_Produto, quantidade, valor_compra, id_embalagem) VALUES (%s, %s, %s, %s, %s)'
+
+def registrar_Pedido(dataCompra, sku, quantidade, embalagem):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    print('parametros: {} {} {} {} {}'.format(dataCompra, sku, quantidade, 0, embalagem))
+    cur.execute(SQL, (dataCompra, sku, quantidade, 0, embalagem))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Pedido gravado!')
 
 @route('/', method='POST')
 def send():
     dataCompra = request.forms.get('dataCompra')
     embalagem = request.forms.get('embalagem')
     sku = request.forms.get('SKU')
-    quantidade = request.forms.get('quantidade')    
+    quantidade = request.forms.get('quantidade') 
+    registrar_Pedido(dataCompra, sku, quantidade, embalagem)   
     return 'Pedido cadastrado! Data: {}, Produto: {}, Quantidade: {}, Embalagem: {}'.format(
         dataCompra, sku, quantidade, embalagem
     )
--- Projetos/Cadastro de pedidos com Docker/scripts/init.sql
+++ Projetos/Cadastro de pedidos com Docker/scripts/init.sql
@@ -24,11 +24,16 @@ create table embalagem (
 
 create table pedido (
     id_pedido serial not null,
-    data_compra timestamp not null,
+    data_compra date not null,
     id_Produto varchar(6) not null,
     quantidade integer not null,
     valor_compra decimal not null,
-    id_embalagem varchar(6) not null,
+    id_Embalagem varchar(6) not null,
     FOREIGN KEY (id_Produto) REFERENCES produto (id_Produto),
     FOREIGN KEY (id_Embalagem) REFERENCES embalagem (id_Embalagem)
 );
+
+insert into produto (id_Produto, nome, preco_venda) values ('JF0001', 'Boneca Baribie', 100.00);
+insert into produto (id_Produto, nome, preco_venda) values ('JF0002', 'Pelúcia Dora Aventureira', 39.99);
+
+insert into embalagem (id_Embalagem, tamanho, quantidade, custo_unitario) values ('JFE001', '15x21', 100, 0.17);
