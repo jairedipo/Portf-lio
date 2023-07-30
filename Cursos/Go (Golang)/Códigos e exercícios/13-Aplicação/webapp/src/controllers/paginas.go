244c3bf5dee705ea2f2b4eee40d8dedc7a6cfcee
Jair Edipo <edipo.jair@aluno.ufabc.edu.br>
2023-07-30 11:53:40 -0300

    Início do desenvolvimento da aplicação web, páginas de login e cadastro de usuários

---
+++ Cursos/Go (Golang)/Códigos e exercícios/13-Aplicação/webapp/src/controllers/paginas.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http"
+	"webapp/src/utils"
+)
+
+// CarregarTelaDeLogin redenriza a tela de login
+func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "login.html", nil)
+}
+
+// CarregarPaginaDeCadastroDeUsuario redenriza a página de cadastro de usuário
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
